1e0938dd129cd2867bf2fe77b795786f4a2afd48
Timothy Asigbey <asigbeytimothy@gmail.com>
2022-02-23 13:40:19 +0000

    port changess

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const {ApolloServer} = require('apollo-server');
 const mongoose = require('mongoose');
 const processEnv = require('dotenv');
 const typeDefs = require('./graphql/TypeDefs/typeDefs');
-const resolvers = require('./graphql/Resolvers/resolvers');  
-processEnv.config({path:'./process.env'});
+const resolvers = require('./graphql/Resolvers/resolvers');
+const config = require('./config');  
+processEnv.config();
 
 
 const server = new ApolloServer({
@@ -12,7 +13,7 @@ const server = new ApolloServer({
     context: ({req}) => ({req}) 
 });
 
-mongoose.connect(`${process.env.DATABASE_URL}`,{useNewUrlParser: true})
+mongoose.connect(config.DATABASE_URL,{useNewUrlParser: true})
 .then(() => {
     console.log('Database Connected successfully');
     return server.listen({port: process.env.PORT || 5000}); 
